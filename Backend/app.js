5bb30905e26851b178d93a13e8c188a356f41684
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-11-03 23:08:29 +0100

    refactor : use get method

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,10 @@
-const http = require( 'http' )
 const express = require( 'express' )
 const app = express()
 
 const Port = process.env.Port || 6500
 
+app.get( '/', ( req, res ) => {
+    res.send('API start')
+})
 
-const server = http.createServer((app)=>{
-   
-}) 
-
-server.listen(Port, ()=>console.log(`server is running to port http://localhost:${Port}`))
+app.listen(Port, ()=>console.log(`server is running to port http://localhost:${Port}`))
