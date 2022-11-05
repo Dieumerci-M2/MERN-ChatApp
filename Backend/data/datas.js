e4c264313aa7786f408dd18923375167dd2476c9
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-11-06 00:40:25 +0100

    feat : add a obect to show object container on the DOM

---
+++ Backend/data/datas.js
@@ -0,0 +1,16 @@
+const datas = [ {
+    id: 1,
+    nom: 'Dieumerci',
+    pstNom: 'Muhindo' ,
+    Email: "dieumerci.md09@gmail.com",
+    numtel : '+243973284730'
+},
+    {
+    id: 2,
+    nom: 'Dieume',
+    pstNom: 'Musengu' ,
+    Email: "dieumerci.md09@gmail.com",
+    numtel : '+243973284730'
+    } ]
+
+module.exports = datas
