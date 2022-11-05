b800a674c4d878d3f3f170d4b0f278da7c1411d2
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-11-06 00:50:51 +0100

    feat : add axios to fetch data comes on Beckend

--- Frontend/src/Pages/Dashboard.jsx
+++ Frontend/src/Pages/Dashboard.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Button from '@mui/material/Button'
+import axios from 'axios'
 
 const Dashboard = () => {
+  const fetchData = async() => {
+    const { data } = await axios.get( '/api/chats' )
+    console.log(data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+  
   return (
     <div>
       <Button variant="contained">Hello Chats</Button>
