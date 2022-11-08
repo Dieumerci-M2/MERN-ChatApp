76d9ce123fc6c229cabcf2abd4daef7f2aba33a5
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-11-08 11:13:08 +0100

    refactor : add type of password and a yes Icon for showing password

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Stack, Button, TextField, Container } from '@mui/material'
+import { Stack, Button, TextField, Container, InputAdornment } from '@mui/material'
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 
 const Login = () => {
   return (
     <Container maxWidth="sm">
-      <Stack spacing = {2} mt={20} p={5} color='warning' sx ={{backgroundColor: 'white', borderRadius: '10px', width: 300, }}>
+      <Stack spacing = {2} mt={20} p={5} color='warning' sx ={{backgroundColor: 'white', borderRadius: '10px', }}>
         <TextField label='Email' variant='outlined' size='small'></TextField>
-        <TextField label='Password' variant='outlined' size='small'></TextField>
+        <TextField label='Password' variant='outlined' size='small' type='password' InputProps={{endAdornment: <InputAdornment position='end'><Button variant='text' endIcon={<RemoveRedEyeIcon/>}></Button></InputAdornment>}}></TextField>
         <Button variant='contained'>Connexion</Button>
         <Button variant='text' >Mot de passe oublié</Button>
         <Button variant='outlined'>Sing-Up</Button>
