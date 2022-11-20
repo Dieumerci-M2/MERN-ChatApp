97a7737eda48c459d7d5858c7ac282cebceb69dd
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-11-20 21:24:40 +0100

    feat :  add Message component

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -10,7 +10,9 @@ import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+import Checkbox from '@mui/material/Checkbox';
+import Favorite from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
@@ -46,7 +48,7 @@ const Sidebar = () => {
             <MoreVertIcon />
           </IconButton>
         }
-        title="Shrimp and Chorizo Paella"
+        title="Dieumerci Md"
         subheader={Date.now()}
       />
       <CardMedia
@@ -62,7 +64,7 @@ const Sidebar = () => {
       </CardContent>
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
+           <Checkbox icon={ <FavoriteBorder /> } checkedIcon={ <Favorite sx={ {color:'red'} } /> } />
         </IconButton>
         <IconButton aria-label="share">
           <ShareIcon />
@@ -79,6 +81,9 @@ const Sidebar = () => {
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
           <Typography paragraph>Biographie:</Typography>
+        </CardContent>
+        <CardContent>
+          <Typography paragrap>My name is Dieumerci MUHINDO I'm a fullstack-Developper and a Beginner in Machine Learning...</Typography>
         </CardContent>
       </Collapse>
     </Card>
