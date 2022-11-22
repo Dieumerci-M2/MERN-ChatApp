8b7544948769f05d3febacb904159c2cb0cefb13
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-11-22 23:36:20 +0100

    feat : add a person on the group

--- Backend/controllers/ChatController.js
+++ Backend/controllers/ChatController.js
@@ -151,6 +151,29 @@ const deleteGroup = async ( req, res ) => {
   }
  }
 
- 
+const addPersonGroup = async ( req, res ) => {
+     const { chatId, userId } = req.body;
+
+  // check if the requester is admin
+
+  const added = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $push: { users: userId },
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!added) {
+    res.status(404);
+    throw new Error("Chat Not Found");
+  } else {
+    res.json(added);
+  }
+}
 
 module.exports = {chatEnter, chatOut, createGroup, renameGroup, deleteGroup}
