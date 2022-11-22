66623d27d0a1ca8db389f04a291978bc783d8724
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-11-22 23:34:55 +0100

    feat : Rename Group

--- Backend/Routes/ChatRoute.js
+++ Backend/Routes/ChatRoute.js
@@ -2,13 +2,13 @@ const express = require( 'express' )
 const router = express.Router()
 const protecte = require('../middlewares/AuthMiddleware')
 const { route } = require( './UserRoute' )
-const {chatEnter, chatOut, createGroup} = require('../controllers/ChatController')
+const {chatEnter, chatOut, createGroup, remaneGroup, deleteGroup} = require('../controllers/ChatController')
 
 router.post( '/',protecte, chatEnter )
 router.get( '/', protecte, chatOut )
 router.post( '/group', protecte, createGroup )
-// router.put( '/grouprename', protecte, remanegroup )
-// router.delete( '/delegroup', protecte, deleteGroup )
-// router.post('/add', protecte, addPersonGroup)
+router.put( '/grouprename', protecte, remaneGroup )
+router.delete( '/delegroup', protecte, deleteGroup )
+router.post('/add', protecte, addPersonGroup)
 
 module.exports = router
--- Backend/controllers/ChatController.js
+++ Backend/controllers/ChatController.js
@@ -103,4 +103,54 @@ const chatOut = ( req, res ) => {
   }
  }
 
-module.exports = {chatEnter, chatOut, createGroup}
+const renameGroup = async ( req, res ) => {
+     const { chatId, chatName } = req.body;
+
+  const updatedChat = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      chatName: chatName,
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!updatedChat) {
+    res.status(404);
+    throw new Error("Chat Not Found");
+  } else {
+    res.json(updatedChat);
+  }
+ }
+
+const deleteGroup = async ( req, res ) => {
+     const { chatId, userId } = req.body;
+
+  // check if the requester is admin
+
+  const removed = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $pull: { users: userId },
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!removed) {
+    res.status(404);
+    throw new Error("Chat Not Found");
+  } else {
+    res.json(removed);
+  }
+ }
+
+ 
+
+module.exports = {chatEnter, chatOut, createGroup, renameGroup, deleteGroup}
