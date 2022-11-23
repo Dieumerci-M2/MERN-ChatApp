7e73d71b59a9abe8e5422e234bc650d97fff3aa1
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-11-23 15:24:37 +0100

    refactor : Change Feed dimension

--- Frontend/src/components/Feed.jsx
+++ Frontend/src/components/Feed.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import { Box } from '@mui/material'
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Post from './Post';
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 const Feed = () => {
-  return (
-    <Box sx={{backgroundColor:'beige'}} flex={9} p={2}>
-      Feed
-    </Box>
-  )
-}
 
+  return (
+    <Box flex = { 6 } p={ 2 }>
+      <Post />
+     
+    </Box>
+  );
+}
 export default Feed
